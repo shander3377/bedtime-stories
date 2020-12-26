c03bbfc2aa93bb5a51c70daa765246e6fdc3eee9
shander3377 <pankaj3377@gmail.com>
2020-12-26 15:42:37 +0530

    login page

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import * as React from "react";
 import { StyleSheet, Text, View, Image } from "react-native";
 import TransactionScreen from "./screens/WriteStoryScreen.js";
 import SearchScreen from "./screens/ReadStoryScreen.js";
+import LoginScreen from './screens/LoginScreen.js';
 import { createBottomTabNavigator } from "react-navigation-tabs";
 import { createAppContainer } from "react-navigation";
 export default class App extends React.Component {
@@ -13,6 +14,12 @@ export default class App extends React.Component {
 const tabNavigator = createBottomTabNavigator({
   Transaction: { screen: TransactionScreen },
   Search: { screen: SearchScreen },
+  
 });
+const switchNavigator = createSwitchNavigator({
+  Login: {screen: LoginScreen},
+  tabNavigator: {screen: tabNavigator}
 
-const AppContainer = createAppContainer(tabNavigator);
+})
+const AppContainer = createAppContainer(switchNavigator)
+
